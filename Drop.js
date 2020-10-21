77a07c8ba27eabf69c8d5f067eefbf3fd29cb5e6
Aryandutta1228 <62468759+Aryandutta1228@users.noreply.github.com>
2020-10-21 18:37:08 +0530

    Add files via upload

---
+++ Drop.js
@@ -0,0 +1,30 @@
+class Drop {
+    constructor(x,y){
+    var options = {
+     
+     friction :0.001,
+     restitution : 0.1
+     
+   }
+    this.drop = Bodies.circle(x,y,5,options);
+    this.radius = 5;
+    
+    World.add(world, this.drop);
+ }
+
+    updateY(){
+        if(this.drop.position.y > height){
+            Matter.Body.setPosition(this.drop, {x:random(20,400), y:random(20,400)})
+        }
+    }
+
+    
+   showDrop(){
+
+    fill("blue");
+    ellipseMode(CENTER);
+    ellipse(this.drop.position.x,this.drop.position.y, this.radius, this.radius);
+    
+    }
+}
+
